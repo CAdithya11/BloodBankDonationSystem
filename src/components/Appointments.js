83f3df70cc80aa18f0be0eead54ad6249ef6ed56
CAdithya11 <143675687+CAdithya11@users.noreply.github.com>
2024-09-27 20:46:42 +0530

    Time Added

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -1,37 +1,10 @@
 import { useEffect, useState } from 'react';
 import AppointmentServices from '../Service/AppointmentService';
 import ScheduleAppointment from './scheduleAppointment';
-import { Select } from 'antd';
 
 function Appointments() {
   const [fetchedAppointments, setFetchedAppointments] = useState([]);
   const [buttonClicked, setButtonClicked] = useState(false);
-  const [time, setTime] = useState([]);
-  const [availableTimes, setAvailableTimes] = useState([]);
-
-  const available = [
-    { label: '08:00:00 am', value: '08:00:00' },
-    { label: '09:00:00 am', value: '09:00:00' },
-    { label: '10:00:00 am', value: '10:00:00' },
-    { label: '11:00:00 am', value: '11:00:00' },
-    { label: '01:00:00 pm', value: '13:00:00' },
-    { label: '02:00:00 pm', value: '14:00:00' },
-    { label: '03:00:00 pm', value: '15:00:00' },
-    { label: '04:00:00 pm', value: '16:00:00' },
-    { label: '05:00:00 pm', value: '17:00:00' },
-  ];
-
-  const disabled = [{ time: '08:00:00' }, { time: '09:00:00' }];
-
-  useEffect(() => {
-    const disabledTimes = disabled.map((item) => item.time);
-    const Time = available.filter((time) => !disabledTimes.includes(time.value));
-    setAvailableTimes(Time);
-  }, [setAvailableTimes, disabled, available]);
-
-  const getValue = (time) => {
-    setTime(time);
-  };
 
   const fetchAppointments = async () => {
     try {
--- src/components/scheduleAppointment.js
+++ src/components/scheduleAppointment.js
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr';
 import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AppointmentServices from '../Service/AppointmentService';
+import { Select } from 'antd';
 
 function ScheduleAppointment() {
   const [location, setLocation] = useState('');
@@ -12,11 +13,33 @@ function ScheduleAppointment() {
 
   const [fetchedLocations, setFetchedLocations] = useState([]);
   const [fetchedDates, setFetchedDates] = useState([]);
+  const disabled = [{ time: '08:00:00' }, { time: '09:00:00' }];
   const [fetchedTimes, setFetchedTimes] = useState([]);
 
-  /*################################################### */
-  /*################# Fetch Location ################## */
-  /*################################################### */
+  const [availableTimes, setAvailableTimes] = useState([]);
+
+  const available = [
+    { label: '08:00:00 am', value: '08:00:00' },
+    { label: '09:00:00 am', value: '09:00:00' },
+    { label: '10:00:00 am', value: '10:00:00' },
+    { label: '11:00:00 am', value: '11:00:00' },
+    { label: '01:00:00 pm', value: '13:00:00' },
+    { label: '02:00:00 pm', value: '14:00:00' },
+    { label: '03:00:00 pm', value: '15:00:00' },
+    { label: '04:00:00 pm', value: '16:00:00' },
+    { label: '05:00:00 pm', value: '17:00:00' },
+  ];
+
+  useEffect(() => {
+    const disabledTimes = disabled.map((item) => item.time);
+    const Time = available.filter((time) => !disabledTimes.includes(time.value));
+    setAvailableTimes(Time);
+  }, [setAvailableTimes, disabled, flatpickr]);
+
+  const getValue = (time) => {
+    setTime(time);
+  };
+
   /* Fetch Locations */
   const fetchLocation = async () => {
     try {
@@ -26,10 +49,6 @@ function ScheduleAppointment() {
       console.error('Error Fetching Location', error);
     }
   };
-
-  /*################################################### 
-    ################## Fetch Dates #################### 
-    ################################################### */
 
   /* Fetch Dates under Location */
   const fetchDate = useCallback(async (location) => {
@@ -42,19 +61,20 @@ function ScheduleAppointment() {
   }, []);
 
   /* Show FlatPicker Dates */
-  flatpickr('#datePicker', {
-    enable: fetchedDates,
-    dateFormat: 'Y-m-d',
-    defaultDate: fetchedDates[0],
-    onChange: (e, dateStr) => {
-      setDate(dateStr);
-    },
-  });
+  useEffect(() => {
+    if (fetchedDates.length > 0) {
+      flatpickr('#datePicker', {
+        disable: fetchedDates,
+        dateFormat: 'Y-m-d',
+        defaultDate: fetchedDates[0],
+        onChange: (e, dateStr) => {
+          setDate(dateStr);
+        },
+      });
+    }
+  }, [fetchedDates]);
 
-  /*################################################### 
-    ################## Fetch Times #################### 
-    ################################################### */
-
+  /* Fetch Times */
   const fetchTime = useCallback(async (location, date) => {
     try {
       const response = await AppointmentServices.findAllAvailableTimesUnderBothDateAndLocationSelection(location, date);
@@ -79,17 +99,27 @@ function ScheduleAppointment() {
   const createAnAppoinment = async (e) => {
     if (!location) {
       alert('Select a location first');
+    } else {
+      if (!date) {
+        alert('Select a date first');
+      } else {
+        if (!time) {
+          alert('Select a time first');
+        } else {
+          try {
+            const appoinment = { location, date, time, status };
+            await AppointmentServices.createAppoinment(appoinment);
+            alert('Succusfully created an Appoinment');
+            navigate('/Appointments');
+          } catch (error) {
+            console.error(error);
+            alert('Failed to created an Appoinment');
+          }
+        }
+      }
     }
+
     e.preventDefault();
-    try {
-      const appoinment = { location, date, time, status };
-      await AppointmentServices.createAppoinment(appoinment);
-      alert('Succusfully created an Appoinment');
-      navigate('/Appointments');
-    } catch (error) {
-      console.error(error);
-      alert('Failed to created an Appoinment');
-    }
   };
 
   return (
@@ -125,34 +155,29 @@ function ScheduleAppointment() {
                 required
                 type="text"
                 value={date}
+                readOnly={fetchedLocations === 0}
                 onChange={(e) => {
                   setDate();
                 }}
                 id="datePicker"
                 placeholder="Date"
+                onClick={(e) => {
+                  if (!fetchedDates.length > 0) {
+                    alert('Pick a location at first');
+                  }
+                }}
               />
             </div>
             <div className="time">
-              <select value={time} required id="time" name="time" onChange={(e) => setTime(e.target.value)}>
-                <option value="" disabled>
-                  Time
-                </option>
-                {fetchedTimes.length > 0 ? (
-                  <>
-                    {fetchedTimes.map((time, index) => {
-                      return (
-                        <option required key={index + 1}>
-                          {time}
-                        </option>
-                      );
-                    })}
-                  </>
-                ) : (
-                  <option value="" disabled selected>
-                    Time
-                  </option>
-                )}
-              </select>
+              <Select
+                required
+                style={{ width: '200px', fontSize: '20px' }}
+                placeholder="Time"
+                allowClear
+                size="large"
+                options={availableTimes}
+                onChange={getValue}
+              />
             </div>
             <button className="saSubmit" type="submit">
               Book
